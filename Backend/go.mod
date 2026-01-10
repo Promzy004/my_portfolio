5c0239e4989063252cf9db91d00f8fdf29966923
Promzy004 <promiseedwin242@gmail.com>
2026-01-10 18:56:16 +0100

    about me page content updated

--- Backend/go.mod
+++ Backend/go.mod
@@ -1,3 +1,5 @@
 module github.com/Promzy004/my_portfolio
 
 go 1.25.0
+
+require github.com/go-chi/chi/v5 v5.2.3
--- Backend/main.go
+++ Backend/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/go-chi/chi/v5"
+)
+
 func main() {
-	// Entry point of the backend application
+	r := chi.NewRouter()
+
+	// Serve frontend static files + React Router fallback
+	r.HandleFunc("/*", func(w http.ResponseWriter, r *http.Request) {
+		distDir := "./dist"
+		path := filepath.Join(distDir, r.URL.Path)
+
+		// Serve index.html for React routes if file doesn't exist
+		if _, err := os.Stat(path); os.IsNotExist(err) {
+			http.ServeFile(w, r, filepath.Join(distDir, "index.html"))
+			return
+		}
+
+		// Otherwise serve the static file
+		http.FileServer(http.Dir(distDir)).ServeHTTP(w, r)
+	})
+
+	fmt.Printf("Server running on [http://127.0.0.1:8000]")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
